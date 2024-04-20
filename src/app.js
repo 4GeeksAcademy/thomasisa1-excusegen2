f7c6d1adfad781991a0bdad6174392e1dce8bfa8
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-04-20 02:14:03 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    //Randomly select one item from eacharray
+    let whoRandom = who[Math.floor(Math.random() * who.length)];
+    let actionRandom = action[Math.floor(Math.random() * action.length)];
+    let whatRandom = what[Math.floor(Math.random() * what.length)];
+    let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+    //Put together the excuse string
+    let excuse =
+      whoRandom +
+      " " +
+      actionRandom +
+      " " +
+      whatRandom +
+      " " +
+      whenRandom +
+      ".";
+    document.getElementById("excuse").innerHTML = excuse;
+
+    return excuse;
+  }
+
+  document.getElementById("excuse").innerHTML = excuse;
+  const target = document.querySelector("#excuse");
+  const button = document.getElementById("button");
+  button.onclick = generateExcuse;
+  console.log(target.children);
+  target.innerHTML = generateExcuse();
 };
